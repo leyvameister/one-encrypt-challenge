8ae2a721e0c922bf2729429245536298e747d1f3
leyvameister <140881301+leyvameister@users.noreply.github.com>
2024-02-02 01:21:06 -0800

    Update
    Added copy button

--- app.js
+++ app.js
@@ -1,21 +1,70 @@
-//OBSOLETE
-
 const normalTextArea = document.getElementById('normal');
 const encryptedTextArea = document.getElementById('encriptado');
 const button = document.querySelector('.btn');
 const errorDiv = document.getElementById('error_message');
 
+let arrowDirection = '';
 
-function decrypt(text) {
-    text = text.replace(/enter/g, "e");
-    text = text.replace(/imes/g, "i");
-    text = text.replace(/ai/g, "a");
-    text = text.replace(/ober/g, "o");
-    text = text.replace(/ufat/g, "u");
+function handleClick() {
 
-    return text;
+    if (arrowDirection == 'right') {
+        const normalTextAreaValue = normalTextArea.value;
+
+        if (!isValidString(normalTextAreaValue)) {
+            displayInvalidText();
+            return;
+        }
+
+        displayInTextArea(encryptedTextArea, encrypt(normalTextAreaValue));
+    }
+
+    if (arrowDirection == 'left') {
+        const encryptedTextAreaValue = encryptedTextArea.value;
+
+        if (!isValidString(encryptedTextAreaValue)) {
+            displayInvalidText();
+            return;
+        }
+
+        displayInTextArea(normalTextArea, decrypt(encryptedTextAreaValue));
+    }
+
 }
 
+function handleClickCopyNormal(){
+    const copyNormalBtn = document.getElementById('copy_normal_btn');
+    navigator.clipboard.writeText(normalTextArea.value);
+    copyNormalBtn.innerHTML = 'Copied';
+    setTimeout(() => {
+        copyNormalBtn.innerHTML = 'Copy';
+    }, 2000);
+
+}
+
+function handleClickCopyEncrypted(){
+    const copyEncryptedBtn = document.getElementById('copy_encrypted_btn');
+    navigator.clipboard.writeText(encryptedTextArea.value);
+    copyEncryptedBtn.innerHTML = 'Copied';
+    setTimeout(() => {
+        copyEncryptedBtn.innerHTML = 'Copy';
+    }, 2000);
+}
+
+function handleNormalInput() {
+    if (invalidTextDisplayed()) {
+        hideInvalidText();
+    }
+    enableButton('right');
+    arrowDirection = 'right';
+}
+
+function handleEncryptedInput() {
+    if (invalidTextDisplayed()) {
+        hideInvalidText();
+    }
+    enableButton('left');
+    arrowDirection = 'left';
+}
 
 function encrypt(text) {
     text = text.replace(/e/g, "enter");
@@ -27,93 +76,48 @@ function encrypt(text) {
     return text;
 }
 
-function handleClick() {
-    if (clearButtonDisplayed()) {
-        clearTextArea(normalTextArea);
-        clearTextArea(encryptedTextArea);
-        handleInput();
-    } else if (hasText(normalTextArea)) {
-        const normalTextAreaValue = normalTextArea.value;
+function decrypt(text) {
+    text = text.replace(/enter/g, "e");
+    text = text.replace(/imes/g, "i");
+    text = text.replace(/ai/g, "a");
+    text = text.replace(/ober/g, "o");
+    text = text.replace(/ufat/g, "u");
 
-        if (!isValidString(normalTextAreaValue)) {
-            displayInvalidText();
-            return;
-        }
-
-        displayInTextArea(encryptedTextArea, encrypt(normalTextAreaValue));
-        handleInput();
-    } else if (hasText(encryptedTextArea)) {
-        const encryptedTextAreaValue = encryptedTextArea.value;
-
-        if (!isValidString(encryptedTextAreaValue)) {
-            displayInvalidText();
-            return;
-        }
-
-        displayInTextArea(normalTextArea, decrypt(encryptedTextAreaValue));
-        handleInput();
-    }
-
+    return text;
 }
 
-function handleInput() {
-
-    if (invalidTextDisplayed()) {
-        hideInvalidText();
-    }
-
-    if (!hasText(normalTextArea) && !hasText(encryptedTextArea)) {
-        disableButton(button);
-    } else if (hasText(normalTextArea) && !hasText(encryptedTextArea)) {
+function enableButton(direction) {
+    if (direction == 'right') {
         button.removeAttribute('disabled');
-        button.innerHTML = '&#8680;'; //right arrow symbol
-    } else if (!hasText(normalTextArea) && hasText(encryptedTextArea)) {
+        button.innerHTML = '&#8680;';
+    } else if (direction == 'left') {
         button.removeAttribute('disabled');
-        button.innerHTML = '&#8678;'; //left arrow symbol
+        button.innerHTML = '&#8678;';
     } else {
-        //both contain text
-        //disableButton(button);
-        button.innerHTML = '&#10006;'; //clear symbol
+        throw new Error('only right or left accepted');
     }
 
-
-}
-
-
-function hasText(textarea) {
-    return textarea.value.length > 0;
-}
-
-function disableButton(button) {
-    button.setAttribute('disabled', true);
-    button.innerHTML = '';
 }
 
 function isValidString(inputString) {
-    const pattern = /^[a-z0-9\s.,!?]+$/;
+    /* const pattern = /^[a-z0-9\s.,!?]+$/; */
+    /* const pattern = /[0-9a-z]+$/; */
+    const pattern = /^[a-z0-9\s]+$/;
     return pattern.test(inputString);
-}
-
-function displayInvalidText() {
-    errorDiv.innerHTML = 'Invalid text';
-}
-
-function hideInvalidText() {
-    errorDiv.innerHTML = '';
-}
-
-function clearButtonDisplayed() {
-    return hasText(normalTextArea) && hasText(encryptedTextArea);
-}
-
-function clearTextArea(textArea) {
-    textArea.value = '';
 }
 
 function displayInTextArea(textArea, value) {
     textArea.value = value;
 }
 
+function displayInvalidText() {
+    errorDiv.innerHTML = 'Invalid text';
+}
+
 function invalidTextDisplayed() {
     return errorDiv.textContent != '';
+}
+
+function hideInvalidText() {
+    errorDiv.innerHTML = '';
 }
